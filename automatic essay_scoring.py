c0865419387b0d9334ae1c3aa2cddfd235a64b4a
dailiyang <liyang.dai@liepin.com>
2019-11-10 16:35:27 +0800

    update

--- automatic essay_scoring.py
+++ automatic essay_scoring.py
@@ -2,10 +2,11 @@ import csv
 import re
 import nltk
 from torch import nn
-from torch.nn.utils.rnn import pack_sequence, PackedSequence
+from torch.nn.utils.rnn import pack_sequence, PackedSequence, pad_packed_sequence, pad_sequence, pack_padded_sequence
 import torch
-from torch.utils.data import DataLoader, TensorDataset, Dataset
+from torch.utils.data import DataLoader, Dataset
 import numpy as np
+import pickle as pk
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -38,17 +39,16 @@ def create_vocab(essay_list):
 
 def read_dataset(file_path):
     # 读取数据
-    features, labels = [], []
+    feature_list, label_list = [], []
     with open(file_path, mode='r', encoding='utf-8') as f:
         raw_data = csv.reader(f, delimiter=',')
         essay_list = []
         for i, line in enumerate(raw_data):
             if i == 0:
                 continue
-            essay_ = line[2].strip()
-            labels.append(int(line[6]))
-            essay_ = essay_.lower()
-            essay = tokenize(essay_)
+            label_list.append(int(line[6]))
+            _essay = line[2].strip().lower()
+            essay = tokenize(_essay)
             essay_list.append(essay)
         word_index, vocab_size = create_vocab(essay_list)
         for essay in essay_list:
@@ -56,27 +56,33 @@ def read_dataset(file_path):
             for word in essay:
                 if word in word_index:
                     indices.append(word_index[word])
-            features.append(torch.tensor(indices, device=device))
-    return features, torch.tensor(labels, dtype=torch.float, device=device), vocab_size
+            feature_list.append(torch.tensor(indices, device=device))
+    return feature_list, torch.tensor(label_list, dtype=torch.float, device=device), vocab_size, word_index
 
 
 class AesNet(nn.Module):
 
-    def __init__(self, vocab_size, embedding_dim, hidden_size, max_score, min_score):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, max_score, min_score, embedding_weight):
         super(AesNet, self).__init__()
         self.max_score = max_score
         self.min_score = min_score
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.embeddings.weight = nn.Parameter(embedding_weight, requires_grad=False)
         self.lstm = nn.LSTM(embedding_dim, hidden_size)
         self.linear = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+        self.cnn = nn.Conv1d(embedding_dim, hidden_size, 2)
 
     def forward(self, inputs):
-        emb = self.embeddings(inputs.data)
-        emb = PackedSequence(emb, inputs.batch_sizes)
-        output, (h, c) = self.lstm(emb)
-        output = self.linear(h)
-        # out[out > self.max_score] = self.max_score
-        # out[out < self.min_score] = self.min_score
+        embeddings = self.embeddings(inputs.data)
+        output = self.cnn(embeddings)
+        output, (h, c) = self.lstm(embeddings)
+        output = pad_packed_sequence(output)
+        output = output[0].sum(0) / output[1].view(-1, 1)
+        output = self.linear(output)
+        output = self.sigmoid(output)
+        # output[output > self.max_score] = self.max_score
+        # output[output < self.min_score] = self.min_score
         return output.view(-1)
 
 
@@ -105,11 +111,11 @@ def collate(batch):
 
 
 def kappa(a, b, max_val, min_val):
-    a = np.round(a)
-    b = np.round(b)
     max_val = int(max_val)
     min_val = int(min_val)
-    return _kappa(a, b,max_val, min_val )
+    a = np.round(a * (max_val - min_val) + min_val)
+    b = np.round(b * (max_val - min_val) + min_val)
+    return _kappa(a, b, max_val, min_val)
 
 
 def _kappa(a, b, max_val, min_val):
@@ -128,15 +134,47 @@ def _kappa(a, b, max_val, min_val):
     return val
 
 
+def _get_embedding_dt():
+    embedding_dt = {}
+    f = open('data/aes/glove.6B.50d.txt', encoding='utf-8')
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = torch.as_tensor([float(_) for _ in values[1:]])
+        embedding_dt[word] = coefs
+    f.close()
+    return embedding_dt
+
+
+def get_embedding_weight(vocab_size, word_index):
+    embedding_dt = _get_embedding_dt()
+    embedding_weight = torch.zeros(vocab_size, 50)
+    for word, i in word_index.items():
+        embedding_vector = embedding_dt.get(word)
+        if embedding_vector is not None:
+            embedding_weight[i] = embedding_vector
+    return embedding_weight
+
+
 if __name__ == '__main__':
-    features, labels, vocab_size = read_dataset('data/aes/aes.csv')
+    # features, labels, vocab_size, word_index = read_dataset('data/aes/aes.csv')
+    # with open('data.pkl', 'wb') as f:
+    #     pk.dump([features, labels, vocab_size, word_index], f)
+    with open('data.pkl', 'rb') as f:
+        features, labels, vocab_size, word_index = pk.load(f)
+    # embedding_weight = get_embedding_weight(vocab_size, word_index)
+    # with open('weight.pkl', 'wb') as f:
+    #     pk.dump(embedding_weight, f)
+    with open('weight.pkl', 'rb') as f:
+        embedding_weight = pk.load(f)
     max_score = labels.max().item()
     min_score = labels.min().item()
+    labels = (labels - min_score) / (max_score - min_score)
     train_dataset = AesDataset(features, labels)
     train_data_loader = DataLoader(train_dataset, batch_size=100, shuffle=True, collate_fn=collate)
-    model = AesNet(vocab_size, 200, 50, max_score, min_score)
+    model = AesNet(vocab_size, 50, 50, max_score, min_score, embedding_weight)
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
     for i in range(20):
         for batched in train_data_loader:
             x, y = batched
